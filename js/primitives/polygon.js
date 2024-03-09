43fa2aeca5d2b96229a09687c00552dc524fbbb2
Stratos Papafotiou <spapafot@gmail.com>
2024-03-09 12:31:59 +0200

    find intersections

--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -1,6 +1,34 @@
 class Polygon {
   constructor(points) {
     this.points = points;
+    this.segments = [];
+    for (let i = 1; i <= points.length; i++) {
+      this.segments.push(new Segment(points[i - 1], points[i % points.length]));
+    }
+  }
+
+  static break(poly1, poly2) {
+    const segs1 = poly1.segments;
+    const segs2 = poly2.segments;
+
+    const intersections = [];
+
+    for (let i = 1; i < segs1.length; i++) {
+      for (let j = 1; j < segs2.length; j++) {
+        const int = getIntersection(
+          segs1[i].p1,
+          segs1[i].p2,
+          segs2[j].p1,
+          segs2[j].p2
+        );
+
+        if (int && int.offset != 0 && int.offset != 1) {
+          const point = new Point(int.x, int.y);
+          intersections.push(point);
+        }
+      }
+    }
+    return intersections;
   }
 
   draw(
